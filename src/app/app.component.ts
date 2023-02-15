8dccc9ca065b3d8c6f3ba46b36e7695a8f53e84c
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2023-02-15 16:29:36 +0100

    add cocktail interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Cocktail} from "./interfaces/cocktail.interface";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularProject';
+  cocktail: Cocktail = {
+    name: "Mojito",
+    image: "https://assets.afcdn.com/recipe/20180705/80345_w1024h1024c1cx4150cy1741.webp",
+    description: "Le mojito, prononcé [moˈxito] en espagnol, ou mojito, morito, ou mohito en français[réf. nécessaire], est un cocktail traditionnel de la cuisine cubaine et de la culture de Cuba, à base de rhum, de soda, de citron vert, et de feuilles de menthe fraîche. Inspiré du mint julep, et variante des Ti-punch des Antilles, Daïquiri, et Cuba libre, il est né à Cuba dans les Caraïbes dans les années 1910 (dont il est à ce jour un emblème exotique international). ",
+  }
 }
---
+++ src/app/interfaces/cocktail.interface.ts
@@ -0,0 +1,5 @@
+export interface Cocktail{
+  name: string;
+  image: string;
+  description: string;
+}
