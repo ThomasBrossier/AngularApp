ed3e856f8906dd660846d0f254f2b592b1fce0dd
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2023-02-21 16:30:20 +0100

    shared Module

--- src/app/features/cocktail/cocktail.module.ts
+++ src/app/features/cocktail/cocktail.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {CocktailListComponent} from "./cocktail-container/cocktail-list/cocktail-list.component";
 import {CocktailDetailsComponent} from "./cocktail-container/cocktail-details/cocktail-details.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -8,22 +7,20 @@ import {RouterModule} from "@angular/router";
 import {CocktailContainerComponent} from "./cocktail-container/cocktail-container.component";
 import {CocktailFormComponent} from "./cocktail-container/cocktail-form/cocktail-form.component";
 
-import {FilterPipe} from "../../shared/pipe/filter.pipe";
-
 import {COCKTAIL_ROUTES} from "./cocktail.routes";
+import {SharedModule} from "../../shared/modules/shared.module";
 
 
 
 @NgModule({
   declarations: [
-    FilterPipe,
     CocktailListComponent,
     CocktailDetailsComponent,
     CocktailContainerComponent,
     CocktailFormComponent,
   ],
   imports: [
-    CommonModule, ReactiveFormsModule, FormsModule, RouterModule.forChild(COCKTAIL_ROUTES)
+    SharedModule, ReactiveFormsModule, FormsModule, RouterModule.forChild(COCKTAIL_ROUTES)
   ]
 })
 export class CocktailModule { }
